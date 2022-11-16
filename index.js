38b82a73282494acd052d06b0490b87598accc76
saeyeonLee <108654869+saeyeonLee@users.noreply.github.com>
2022-11-17 00:44:43 +0900

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require("./config/express");
-const { logger } = require("./config/winston"); // log
-
-const port = 3000;
-express().listen(port);
-logger.info(`API Server Start At Port ${port}`);
